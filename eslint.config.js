cc300a327e23139b3718f4122c254e2aa8f900d4
Aleksander Odland Fattnes <Aleksander.odland@lyse.net>
2025-09-27 15:33:28 +0200

    fix: Added missing prettier plugin import

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import prettier from "eslint-plugin-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -17,11 +17,14 @@ export default tseslint.config([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
       globals: globals.browser,
     },
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
-      "prettier/prettier": "error"
-    }
+      'prettier/prettier': 'error',
+    },
   },
-])
+]);
