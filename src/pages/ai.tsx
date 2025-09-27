1c67b69114705d468ca304defe1dc3fcfb100ecb
Aleksander Odland Fattnes <Aleksander.odland@lyse.net>
2025-09-27 03:15:17 +0200

    feat: Added chatbot draft

--- src/pages/ai.tsx
+++ src/pages/ai.tsx
@@ -1,3 +1,9 @@
+import { ChatBot } from "@/components/chatbot/chat-bot"
+
 export default function Ai() {
-  return <div></div>
+  return (
+    <div className="p-6">
+        <ChatBot />
+    </div>
+  )
 }
